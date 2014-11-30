8e027462deb19324cb2ad715b885d112e82af8da
Lasse Spuur Nielsen <lasse.nielsen.dk@gmail.com>
2014-11-30 22:27:38 +0100

    updated xmlparser example

--- examples/source/xmlparser.cpp
+++ examples/source/xmlparser.cpp
@@ -1,28 +1,78 @@
 #include <iostream>
-#include <dpl/symparser.hpp>
+#include <dpl/slrparser.hpp>
+#include <sstream>
+#include <dpl/common.hpp>
 
 using namespace dpl;
 using namespace std;
+
+void print_equals(bool first, int indent, const parsetree *tree, ostream &out) // {{{
+{ if (tree->case_name=="none")
+    ;
+  else if (tree->case_name=="some") // id eq string equals
+  { if (first)
+      out << " ";
+    else
+    { out << endl;
+      for (int i=0; i<indent; ++i)
+        out << " ";
+    }
+    out << tree->content[0]->root.content;
+    out << " = ";
+    out << tree->content[2]->root.content;
+    print_equals(false,indent,tree->content[3],out);
+  }
+  else
+    throw string("Unknown equals case: ") + tree->case_name;
+} // }}}
+void print_tree(bool first, int indent, const parsetree *tree, ostream &out) // {{{
+{ if (tree->case_name=="none")
+    ;
+  else if (tree->case_name=="some") // < id equals > tree < / id > tree
+  { if (!first)
+      out << endl;
+    for (int i=0; i<indent; ++i)
+      out << " ";
+    out << "<" << tree->content[1]->root.content;
+    print_equals(true,indent+2+tree->content[1]->root.content.size(),tree->content[2],out);
+    out << ">";
+    print_tree(false,indent+2,tree->content[4],out);
+    out << endl;
+    for (int i=0; i<indent; ++i)
+      out << " ";
+    out << "</";
+    if (tree->content[1]->root.content!=tree->content[6]->root.content)
+    { pair<int,int> pos = tree->GetPosition();
+      throw string("Start and end tags do not match at line: ") + to_string(pos.first) + ", col: " + to_string(pos.second);
+    }
+    out << tree->content[6]->root.content;
+    out << ">";
+    print_tree(false,indent,tree->content[8],out);
+  }
+  else
+    throw string("Unknown tree case: ") + tree->case_name;
+} // }}}
 
 int main(int argc, char **argv)
 {
   try
   { if (argc<2) { cout << "Syntax: xmlparser \"<xmlstring>\"" << endl; return 1; }
     string xmlstring = argv[1];
-    SymParser parser;
-    parser.DefGeneralToken(""," \n\t");
-    parser.DefToken("id", "[a-z][a-z]*");
-    parser.DefToken("string", "\"[a-zA-Z0-9 ]*\"");
+    SlrParser parser("tree");
+    parser.DefGeneralToken(""," \n\r\f\t");
+    parser.DefToken("id", "[a-zA-Z][a-zA-Z]*");
+    parser.DefToken("string", "\"(\\\\[^]+[^\\\\\"])*\"");
     parser.DefToken("eq","=");
     parser.DefKeywordToken("<",2);
-    parser.DefKeywordToken("</",1);
+    parser.DefToken("</","<[ \n\r\f\t]*/",1);
     parser.DefKeywordToken(">");
-    parser.DefType("branch ::= < id equals > tree </ id >");
-    parser.DefType("equals ::= | id eq string equals");
-    parser.DefType("tree ::= | branch tree");
+    parser.DefType("equals ::= ::none | ::some id eq string equals");
+    parser.DefType("tree ::= ::none | ::some < id equals > tree </ id > tree");
     
     parsetree *tree=parser.Parse(xmlstring);
-    cout << tree->ToString() << endl;
+    stringstream ss;
+    print_tree(true,0,tree,ss);
+    cout << ss.str() << endl;
     delete tree;
   }
   catch (string s)
