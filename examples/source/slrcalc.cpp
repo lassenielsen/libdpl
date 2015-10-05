0ca0e557b0ccb8c61c74536ce77008c8a2232e61
Lasse Nielsen <lasse.nielsen.dk@gmail.com>
2015-10-05 10:53:35 +0200

    Updated slrcalc example

--- examples/source/slrcalc.cpp
+++ examples/source/slrcalc.cpp
@@ -15,15 +15,13 @@ int a2i(const string &a) // {{{
 
 int calc(const parsetree *tree) // {{{
 { // Do the actual calculation
-  if (tree->type_name == "number") return a2i(tree->root.content);
+  if (tree->type_name == "num") return a2i(tree->root.content);
   if (tree->case_name == "add") return calc(tree->content[0]) + calc(tree->content[2]);
   if (tree->case_name == "sub") return calc(tree->content[0]) - calc(tree->content[2]);
-  if (tree->case_name == "sub1") return calc(tree->content[0]);
   if (tree->case_name == "mul") return calc(tree->content[0]) * calc(tree->content[2]);
   if (tree->case_name == "div") return calc(tree->content[0]) / calc(tree->content[2]);
-  if (tree->case_name == "min") return -calc(tree->content[1]);
-  if (tree->case_name == "sub2") return calc(tree->content[0]);
-  if (tree->case_name == "int") return a2i(tree->content[0]->root.content);
+  if (tree->case_name == "num") return calc(tree->content[0]);
+  if (tree->case_name == "lvl") return calc(tree->content[0]);
   if (tree->case_name == "par") return calc(tree->content[1]);
   cout << "Calc error unexpected case: " << tree->type_name << "." << tree->case_name << endl;
   return 0;
@@ -39,21 +37,7 @@ int main(int argc, char **argv) // {{{
     }
 
     string exp = argv[1]; // Copy argument
-    SlrParser parser("eqn"); // Define parser
-    parser.DefToken("","[ \t\r\n][ \t\r\n]*");
-    parser.DefGeneralToken("number", "0123456789");
-    parser.DefKeywordToken("(");
-    parser.DefKeywordToken(")");
-    parser.DefKeywordToken("+");
-    parser.DefKeywordToken("-");
-    parser.DefKeywordToken("~");
-    parser.DefKeywordToken("*");
-    parser.DefKeywordToken("/");
-
-    parser.DefType("eqn ::= ::add eqn + eqn1 | ::sub eqn - eqn1 | ::sub1 eqn1");
-    parser.DefType("eqn1 ::= ::mul eqn1 * eqn2 | ::div eqn1 / eqn2 | ::min ~ eqn2 | ::sub2 eqn2");
-    parser.DefType("eqn2 ::= ::int number | ::par ( eqn )");
-
+    SlrParser parser("exp","defs/exp_sugar.ttd"); // Define parser
     parsetree *tree=parser.Parse(exp); // Parse argument
     // Calculate and print result
     cout << parser.Unparse(*tree) << " = " << calc(tree) << endl;
